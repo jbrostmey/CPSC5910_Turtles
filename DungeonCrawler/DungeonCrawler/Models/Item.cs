1491e4dad3bce52c536eba7bc825f4bf2118405d
jbrostmeyerSU <brostmey@seattleu.edu>
2018-01-24 15:39:57 -0800

    test commit  Please enter the commit message for your changes. Lines starting

---
+++ DungeonCrawler/DungeonCrawler/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DungeonCrawler
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DungeonCrawler/DungeonCrawler/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace DungeonCrawler
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
