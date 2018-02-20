7a73262777cb6f213a1b5f11c1e7e4f76cfc9c83
Denny <Denny@Dennys-MacBook-Pro.local>
2018-02-19 22:04:20 -0800

    Battle Pages added

---
+++ DungeonCrawler/DungeonCrawler/Views/Battle/BattleMessage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DungeonCrawler.Views.Battle
+{
+    public partial class BattleMessage : ContentPage
+    {
+        public BattleMessage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ContinueGame_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BattleOver());
+        }
+    }
+}
---
+++ DungeonCrawler/DungeonCrawler/Views/Battle/BattleOver.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DungeonCrawler.Views.Battle
+{
+    public partial class BattleOver : ContentPage
+    {
+        public BattleOver()
+        {
+            InitializeComponent();
+        }
+
+
+        //Functions below are not functional at this time.
+
+        private async void NextLevel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+
+        private async void OnPickUpItemsButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+
+        private async void OnViewStatistics_ButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
