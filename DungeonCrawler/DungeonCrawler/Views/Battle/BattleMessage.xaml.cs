4875c1b31778dce683d783583708e94220d36191
jbrostmey <brostmey@seattleu.edu>
2018-02-25 14:56:58 -0800

    Battle message implemented

--- DungeonCrawler/DungeonCrawler/Views/Battle/BattleMessage.xaml.cs
+++ DungeonCrawler/DungeonCrawler/Views/Battle/BattleMessage.xaml.cs
@@ -2,29 +2,93 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace DungeonCrawler.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
     public partial class BattleMessage : ContentPage
     {
         public string message;
         //message passed in from Turn
         public BattleMessage(string msg)
         {
-            InitializeComponent();
-            message = msg;
-            this.BindingContext = this;
 
-//message passed correctly. Having troubles with databinding and passing message to XAML 
-            this.Content = new Label
+            Label header = new Label
             {
-                Text = message
-            };
-        }
+                Text = "Battle Message: ",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+                                                 
 
-        private async void ContinueGame_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new BattleOver());
+            };
+            Label message = new Label
+            {
+                Text = msg,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+
+
+            };
+
+            Button button = new Button
+            {
+                Text = "Continue Game",
+                Font = Font.SystemFontOfSize(NamedSize.Medium),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            button.Clicked += ContinueGame_Clicked;
+
+            this.Content = new StackLayout
+            {
+
+                Children =
+                {
+
+                    header,
+                    message,
+                    button
+                }
+            };
+
+
+
+
+            //  InitializeComponent();
+
+            //this.BindingContext = message;
+
+
+
+
+
+
+            //message passed correctly. Having troubles with databinding and passing message to XAML 
+            /* this.Content = new Label
+             {
+                 Text = message
+             }
+         }
+ */
+
+
+            /*      async void ContinueGame_Clicked(object sender, EventArgs e)
+                 {
+                     await Navigation.PushAsync(new BattleOver());
+                 }
+
+         */
+
+            async void ContinueGame_Clicked(object sender, EventArgs e)
+            {
+                await Navigation.PushAsync(new BattleOver());
+            }
         }
     }
 }
